234b3d13992b96ae3ac9ea81ff8c220d3c9b41d1
SemptechVzla <ingsimonmoreno@outlook.com>
2024-12-11 09:54:13 -0400

    estructura de archivos

--- index.php
+++ index.php
@@ -4,15 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Frontendmentor</title>
+    <title>Proyectos PhP</title>
 </head>
 <body>
-    <h1>Php</h1>
+    <h1>Proyectos</h1>
     <main class="container">
         <ul>
-            <li><a href="#">Portafolio</a></li>
-            <li><a href="#"></a>Overlord</li>
-            <li><a href="#"></a>Gatcha</li>              
+            <li><a href="/Proyectos/Portafolio/index.php">Portafolio</a></li>
+            <li><a href="/Proyectos/Overlord/index.php"></a>Overlord</li>                        
         </ul>
     </main>
 </body>
